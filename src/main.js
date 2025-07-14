5c4c3de9badeab88e50396e9b18bf20f66102039
Ejnar40 <Einar.lundqvist@gmail.com>
2025-07-14 21:36:06 +0200

    atempt 10

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// src/main.js
+
+// If you have styles you want Vite to bundle:
+import '../style.css';  
+
+// Then either write your code directly:
+console.log('App starting…');
+
+// Or import your existing script.js logic:
+import '../script.js';
