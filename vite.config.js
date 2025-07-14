abac76af7c52110904f91acacdd56b5312399b77
Ejnar40 <Einar.lundqvist@gmail.com>
2025-07-14 21:08:24 +0200

    atempt 2

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // root: '.',        // default is project root
+  build: {
+    outDir: 'dist',    // default
+    // assetsDir: 'assets',
+  },
+  // resolve: {
+  //   alias: { '@': '/src' }
+  // }
+});
