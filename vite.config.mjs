9d5210214135fdd39582d1d6ec2053a3b5b38178
Ejnar40 <Einar.lundqvist@gmail.com>
2025-07-14 21:17:13 +0200

    atempt 5

---
+++ vite.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html')
+      }
+    }
+  }
+});
